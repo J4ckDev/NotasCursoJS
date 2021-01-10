ae307960ab1297f905d0b888b2a15b5310e220dc
J4ckDev <j4ckdev@gmail.com>
2021-01-09 21:10:55 -0500

    Clase 4 finalizada

---
+++ Basics/2_Condicionales.js
@@ -0,0 +1,60 @@
+/**
+ *  CONDICIONALES
+ */
+
+/**
+ * Invitación a una Fiesta
+ * Si eres mayor de edad y menor de 65 años puedes venir a la fiesta;
+ * sino, si tienes permiso firmado de tus padres puedes venir;
+ * sino quédate en casa.
+ */
+let nombre = "John Doe";
+//let edad = 23;
+//let edad = 67;
+let edad = 15;
+let permiso = true;
+
+if (edad >= 18 && edad < 65) {
+  console.log(`${nombre} puedes venir a la fiesta`);
+} else if (permiso === true && edad < 18) {
+  console.log(`${nombre} puedes venir, tienes permiso`);
+} else {
+  console.log(`${nombre} no puedes venir a la fiesta, quédate en casa`);
+}
+
+/**
+ * CONDICIONALES MÚLTIPLES
+ */
+
+/**
+ * ¿Qué personaje de DragonBall Z eres?
+ *
+ * Si eres fuerte y comelón eres Gokú.
+ * Si eres veloz y egoíste eres Vegeta
+ * Si eres pequeño y débil eres Krilin
+ * Si eres travieso y juguetón eres Trunks
+ * Si no eres ninguno, eres una sabandija.
+ */
+
+let personalidad = "Pequeño y débil";
+
+switch (personalidad) {
+  case "Fuerte y comelón":
+    console.log("Eres Gokú");
+    break;
+
+  case "Veloz y egoísta":
+    console.log("Eres Vegeta");
+    break;
+
+  case "Pequeño y débil":
+    console.log("Eres Krilin");
+    break;
+
+  case "Travieso y juguetón":
+    console.log("Eres Trunks");
+    break;
+
+  default:
+    console.log("Eres una sabandija");
+}
---
+++ Basics/3_Ciclos.js
@@ -0,0 +1,29 @@
+/**
+ * CICLOS, BUCLES O LOOPS
+ */
+
+// for
+
+for (let index = 0; index < 10; index++) {
+  console.log(index);
+}
+
+/**
+ * Ejemplo con for.
+ * Dado un texto deseamos imprimir su contenido letra por letra
+ */
+
+let text = "Ciao Mondo!!";
+
+for (let i = 0; i < text.length; i++) {
+  console.log(text[i]);
+}
+
+/**
+ * while
+ */
+let valor = 0;
+while (valor < 10) {
+    console.log(valor);
+    valor++
+}
