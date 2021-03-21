b93cbf42f948f7e2666d450e5f758462c8f3dbe9
Jonathan Aldana <j4ckdev@gmail.com>
2021-03-21 18:51:46 -0500

    Punto 4 optimizado un poco

--- Basics/Ejercicios/4.Primos/app.js
+++ Basics/Ejercicios/4.Primos/app.js
@@ -1,25 +1,19 @@
 let numero = 0;
-let resultado = 0;
+let resultado = true;
 let dataOut = document.getElementById("Out");
 
 while (numero <= 1) {
     numero =  parseInt(prompt("Ingrese un número mayor a 1 para identificar si es primo."));
 }
 
-for (let i = 1; i <= numero; i++) {
+for (let i = 2; i < numero; i++) {
     if (numero % i == 0) {
-        resultado += 1;
-    } else {
-        resultado += 0;
+        resultado = false;
+        break;
     }
 }
-
-switch (resultado) {
-    case 2:
-        dataOut.textContent = `El ${numero} es primo.`
-        break;
-
-    default:
-        dataOut.textContent = `El ${numero} no es primo.`
-        break;
+if (resultado) {
+    dataOut.textContent = `El ${numero} es primo.`;
+} else {
+    dataOut.textContent = `El ${numero} no es primo.`
 }
