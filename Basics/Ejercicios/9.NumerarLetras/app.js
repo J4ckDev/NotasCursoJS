fbfcd889f8cd93c6b960f4c69f7bbba48169d641
Jonathan Aldana <j4ckdev@gmail.com>
2021-03-25 20:01:02 -0500

    Corrección en el ejercicio 9

--- Basics/Ejercicios/9.NumerarLetras/app.js
+++ Basics/Ejercicios/9.NumerarLetras/app.js
@@ -6,34 +6,33 @@ let numVocales = 0;
 let regexPalabras = /(?<!\-)\b[a-záéíóúüñ]+(?:(?=\s)|(?=\.)|(?=\;)|(?=\,)|(?=\:)|\b)/gi;
 let regexConsonante = /[b-df-hj-np-tv-zñ]/gi;
 let regexVocal = /[aeiouáéíóúü]/gi;
-let dataOut = document.getElementById("Out");
+const dataTitle = document.getElementById("title");
+const dataVocales = document.getElementById("vocales");
+const dataConsonantes = document.getElementById("consonantes");
+const dataLongitud = document.getElementById("longitud");
+const dataOut = document.getElementById("Out");
 
 while (numPalabras != 1) {
   texto = prompt("Ingresa una palabra");
   palabras = texto.match(regexPalabras);
-  if (palabras == null) {
-    continue;
-  } else {
+  if (palabras != null) {
     numPalabras = palabras.length;
-  }
-  if (numPalabras == 1) {
-    break;
   }
 }
 
 if (palabras[0].match(regexVocal) == null) {
-    numVocales = 0;
+  numVocales = 0;
 } else {
-    numVocales = palabras[0].match(regexVocal).length;
+  numVocales = palabras[0].match(regexVocal).length;
 }
 
 if (palabras[0].match(regexConsonante) == null) {
-    numConsonantes = 0;
+  numConsonantes = 0;
 } else {
-    numConsonantes = palabras[0].match(regexConsonante).length;
+  numConsonantes = palabras[0].match(regexConsonante).length;
 }
 
-dataOut.innerHTML = `<h3>Datos de la palabra ${palabras[0]}</h3>`;
-dataOut.innerHTML += `<p>El número de letras es: ${palabras[0].length}</p>`;
-dataOut.innerHTML += `<p>El número de consonantes es: ${numConsonantes}</p>`;
-dataOut.innerHTML += `<p>El número de vocales es: ${numVocales}</p>`;
+dataTitle.textContent = `Datos de la palabra ${palabras[0]}`;
+dataVocales.textContent = `El número de letras es: ${palabras[0].length}`;
+dataConsonantes.textContent = `El número de consonantes es: ${numConsonantes}`;
+dataLongitud.textContent = `El número de vocales es: ${numVocales}`;
